254727af16261845e34b58c0fdbf0c257f942fa5
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-02 14:46:46 +0200

    premiers exercices en C

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - Entry point
+ *
+ * Descritption: 'swap a with b'
+ * @str: is an string
+ *
+ * Return: .
+ */
+void print_rev(char *str)
+{
+	int i = strlen(str);
+
+	while (i >= 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
