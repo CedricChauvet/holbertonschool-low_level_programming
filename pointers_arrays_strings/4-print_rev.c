978e6c6e1fd10852a50dd4250c224f9311bff813
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-02 14:58:33 +0200

    premiers exercices en C

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
+
+
+/**
+ * _strlen - Entry point
+ *
+ * Descritption: 'swap a with b'
+ * @s: is an string
+ *
+ * Return: long int.
+ */
+
+long int _strlen(char *s)
+{
+long int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
+}
+
 /**
  * print_rev - Entry point
  *
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,2 +1,5 @@
+long int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
+
