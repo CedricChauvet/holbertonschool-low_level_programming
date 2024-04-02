77a53d0971c7bbefaa3417c4b77972cfb47ec1f1
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-02 11:55:44 +0200

    premiers exercices en C

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _strlen - Entry point
+ *
+ * Descritption: 'swap a with b'
+ * @s1: is an string
+ *
+ * Return.
+ */
+
+
+int _strlen(char s1[])
+{
+int len = 0;
+len = strlen(s1); 
+return len;   
+}
