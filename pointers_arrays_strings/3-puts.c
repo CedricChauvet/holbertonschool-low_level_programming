4f5da6314015e2b704c299828e18e19fb87c184a
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-02 13:44:33 +0200

    premiers exercices en C

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - Entry point
+ *
+ * Descritption: 'swap a with b'
+ * @str: is an string
+ *
+ * Return: .
+ */
+
+
+
+
+void _puts(char *str){
+    fprintf( stdout, str );
+    fprintf( stdout, "\n" );
+
+}
