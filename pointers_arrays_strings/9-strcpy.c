9458c1b2787bdf79c39facf550a0be0d9c35ca83
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-04 11:01:42 +0200

    bridge jour 8

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,26 @@
 #include <stdlib.h>
 
 /**
+* _strlen - Entry point
+*
+* Descritption: 'swap a with b'
+* @s: is an string
+*
+* Return: long int.
+*/
+
+long int _strlen(char *s)
+{
+long int len = 0;
+
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
+}
+
+/**
 * _strcpy - Entry point
 *
 * Descritption: 'print un caracter sur 2'
@@ -15,15 +35,17 @@
 * Return:a char
 */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 	{
 	size_t i;
-
-	while (src[i] != '\0')
+	int len = _strlen(src);
+	while (len > 0)
 	{
 		dest[i] = src[i];
 		i++;
+		len--;
 	}
+
 	dest[i + 1] = '\0';
 	return (dest);
 }
