bb084770e9079b57ff4b7dc3c5f498049d115647
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-04 10:57:10 +0200

    bridge jour 8

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -19,10 +19,11 @@ char *_strcpy(char *dest, const char *src)
 	{
 	size_t i;
 
-	for ( ; src[i] != '\0'; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	dest[i] = '\0';
+	dest[i + 1] = '\0';
 	return (dest);
 }
