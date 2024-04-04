43e13d386f04f8cc2a3f07722c0417e3a3cc68f6
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-04 13:19:09 +0200

    bridge jour 8

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
-long int _strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
