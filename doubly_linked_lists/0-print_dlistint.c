3ca6cd86af9bf37fdfb47380c3ba5089e0d5fdb7
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-04 17:23:01 +0200

    Nbridge jour 8

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
