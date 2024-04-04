4c6c36188820b7f783d75db05f233c651d0f7a52
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-04 18:26:05 +0200

    bridge jeudi soir

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,15 @@
  * Return: Always EXIT_SUCCESS.
  */
 
+
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t count = 0;
 
+//	while (h != NULL)  {
+        printf("%d\n", h->n);
+//        h = h->next;
+        count++;
+ //   }
+    return count;
 }
